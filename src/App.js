59a2c553264650b2b1f4aa73ed69c51401534619
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-04 20:09:51 +0100

    getting collection

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
+  import { getDocs,collection } from "firebase/firestore";
+import { useEffect } from "react";
+import { db } from "./firebase/firebase";
+
 
 function App() {
+
+
+  useEffect(() => {
+
+    async function test() {
+      
+      const querySnapshot = await getDocs(collection(db, "Child"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+
+    }
+
+    test()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
