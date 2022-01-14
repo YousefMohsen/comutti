19960e3dc9341d5303a3b73e532bd2d045a0170b
AliceCabane <alice.cabane@edu.ece.fr>
2022-01-14 11:38:23 +0100

    ChildSelectionPage

---
+++ src/Admin/Pages/ChildSelectionPage.js
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+
+function ChildSelection() {
+    return (
+      <div className="container">
+        
+        
+          <div>This is the child selection page</div>
+          
+  
+      </div>
+    );
+  }
+  
+  export default ChildSelection;
