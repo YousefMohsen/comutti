4d0cdfd158e7c92c6fa49c2d401c66d917ba80f1
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-23 19:45:49 +0100

    wip

--- src/Child/Stories/QuestionContainer/QuestionContainer.js
+++ src/Child/Stories/QuestionContainer/QuestionContainer.js
@@ -1,12 +1,54 @@
+import { useState } from "react";
 import styles from "./QuestionContainer.module.css";
 
-function QuestionContainer() {
-  return <div className={styles.container}>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>3</button>
-      <button className={styles.button}>4</button>
-  </div>;
+function QuestionContainer({ bt1, bt2, bt3, bt4, correctAnswer, next }) {
+  const [btn1WrongClicked, setBtn1WrongClicked] = useState(false);
+  const [btn2WrongClicked, setBtn2WrongClicked] = useState(false);
+  const [btn3WrongClicked, setBtn3WrongClicked] = useState(false);
+  const [btn4WrongClicked, setBtn4WrongClicked] = useState(false);
+
+  const btnClicked = (number) => {
+    if (number === +correctAnswer) {
+      next();
+    }
+
+    switch (number) {
+      case 1:
+        setBtn1WrongClicked(true);
+        break;
+      case 2:
+        setBtn2WrongClicked(true);
+        break;
+      case 3:
+        setBtn3WrongClicked(true);
+        break;
+      case 4:
+        setBtn4WrongClicked(true);
+        break;
+    }
+  };
+
+  const btn1Classes = [styles.button,(btn1WrongClicked ? styles.wrong : null)];
+  const btn2Classes = [styles.button,(btn2WrongClicked ? styles.wrong : null)];
+  const btn3Classes = [styles.button,(btn3WrongClicked ? styles.wrong : null)];
+  const btn4Classes = [styles.button,(btn4WrongClicked ? styles.wrong : null)];
+
+  return (
+    <div className={styles.container}>
+      <button onClick={() => btnClicked(1)} className={btn1Classes.join('')}>
+        {bt1}
+      </button>
+      <button onClick={() => btnClicked(2)} className={btn2Classes.join('')}>
+        {bt2}
+      </button>
+      <button onClick={() => btnClicked(3)} className={btn3Classes.join('')}>
+        {bt3}
+      </button>
+      <button onClick={() => btnClicked(4)} className={btn4Classes.join('')}>
+        {bt4}
+      </button>
+    </div>
+  );
 }
 
 export default QuestionContainer;
--- src/Child/Stories/StoriesContainer/StoriesContainer.js
+++ src/Child/Stories/StoriesContainer/StoriesContainer.js
@@ -25,38 +25,124 @@ function StoriesContainer() {
   const nextStory = () => {
     setCurrentStory((prev) => prev + 1);
   };
-  
+
   const frames = useMemo(
     () => [
       {
         img: images[0].default,
-      //  component: <StoryText>Text</StoryText>
+        //  component: <StoryText>Text</StoryText>
         // type: "question",
         // answers: ["1", "2", "3", "4"],
         // correctAnswer: 2,
       },
       {
         img: images[1].default,
-        component: <Button y="70%" x="50%" clicked={nextStory}>Touch to start</Button>,
+        component: (
+          <Button y="70%" x="50%" clicked={nextStory}>
+            Touch to start
+          </Button>
+        ),
         // type: "question",
         // answers: ["1", "2", "3", "4"],
         // correctAnswer: 2,
       },
       {
         img: images[2].default,
-        component: <>
-        <Button x="85%" y="70%" color="#058ED9" clicked={nextStory}>Boy</Button>
-        <Button x="20%" y="70%" color="#F49097" clicked={nextStory}>Girl</Button>
-        </>,
+        component: (
+          <>
+            <Button x="85%" y="70%" color="#058ED9" clicked={nextStory}>
+              Boy
+            </Button>
+            <Button x="20%" y="70%" color="#F49097" clicked={nextStory}>
+              Girl
+            </Button>
+          </>
+        ),
         // type: "question",
         // answers: ["1", "2", "3", "4"],
         // correctAnswer: 2,
       },
       {
         img: images[4].default,
-        component: <><QuestionContainer/></>,
-        clickToContinue:true,
+        component: (
+          <StoryText x="50%" y="70%">
+            There is a girl named Marie. Marie have a friend named James at
+            school.
+          </StoryText>
+        ),
+        clickToContinue: true,
         // type: "question",
+        // answers: ["1", "2", "3", "4"],
+        // correctAnswer: 2,
+      },
+      {
+        img: images[4].default,
+        component: (
+          <StoryText x="50%" y="70%">
+            One day James invited Marie over to his house to play.
+          </StoryText>
+        ),
+        clickToContinue: true,
+        // type: "question",
+        // answers: ["1", "2", "3", "4"],
+        // correctAnswer: 2,
+      },
+      {
+        img: images[7].default,
+        component: (
+          <StoryText x="50%" y="70%">
+            When they arrived at James house they sat down on the sofa.
+          </StoryText>
+        ),
+        clickToContinue: true,
+        // type: "question",
+        // answers: ["1", "2", "3", "4"],
+        // correctAnswer: 2,
+      },
+      {
+        img: images[7].default,
+        component: (
+          <StoryText x="50%" y="70%">
+            They were chatting away when suddenly they heard a sound “WOOF
+            WOOF”.
+          </StoryText>
+        ),
+        clickToContinue: true,
+        // type: "question",
+        // answers: ["1", "2", "3", "4"],
+        // correctAnswer: 2,
+      },
+      {
+        img: images[7].default,
+        component: (
+          <StoryText x="50%" y="70%">
+            “How dear ! What’s that I hear, tell me it’s nothing to fear !” says
+            Marie.
+          </StoryText>
+        ),
+        clickToContinue: true,
+        // type: "question",
+        // answers: ["1", "2", "3", "4"],
+        // correctAnswer: 2,
+      },
+      {
+        img: images[7].default,
+        component: (
+          <>
+            <StoryText x="50%" y="20%">
+              How do you think Marie is feeling?
+            </StoryText>
+            <QuestionContainer
+              bt1={<img src={require("../../../images/OtherImages/AngrySmiley.png").default}/>}
+              bt2={<img src={require("../../../images/OtherImages/AnnoyedSmiley.png").default}/>}
+              bt3={<img src={require("../../../images/OtherImages/CryingLaughingSmiley.png").default}/>}
+              bt4={<img src={require("../../../images/OtherImages/SadSmiley.png").default}/>}
+              correctAnswer="1"
+              next={nextStory}
+            />
+          </>
+        ),
+        type: "question",
         // answers: ["1", "2", "3", "4"],
         // correctAnswer: 2,
       },
@@ -66,9 +152,18 @@ function StoriesContainer() {
 
   let content;
   if (frames[currentStory].component) {
-    content = <ImgContainer clicked={frames[currentStory].clickToContinue ? nextStory : null} img={frames[currentStory].img}>{frames[currentStory].component}</ImgContainer>;
+    content = (
+      <ImgContainer
+        clicked={frames[currentStory].clickToContinue ? nextStory : null}
+        img={frames[currentStory].img}
+      >
+        {frames[currentStory].component}
+      </ImgContainer>
+    );
   } else {
-    content = <ImgContainer clicked={nextStory} img={frames[currentStory].img}/>;
+    content = (
+      <ImgContainer clicked={nextStory} img={frames[currentStory].img} />
+    );
   }
 
   return content;
